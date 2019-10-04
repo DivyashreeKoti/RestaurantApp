3d7cdf1b6b624ba079e0a5fd14a85aa3161220ee
priyankasrinivas <priyanka.srinivas@a2-g.com>
2019-10-04 13:52:31 -0400

    intial commit

---
+++ src/main/java/com/divpri/RestaurantHunt/RestaurantHuntApplication.java
@@ -0,0 +1,13 @@
+package com.divpri.RestaurantHunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantHuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantHuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divpri/RestaurantHunt/RestaurantHuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divpri.RestaurantHunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantHuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
